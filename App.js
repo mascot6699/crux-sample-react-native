6dc32c1ca8ef0cc5282a64397b6032b990f1a883
mascot66993 <umang.shukla@coinswitch.co>
2019-10-22 10:03:34 +0000

    encryption crux pay layer working

--- App.js
+++ App.js
@@ -1,61 +1,59 @@
 import React, { Component } from "react";
-import { AsyncStorage as localStorage } from "react-native";
-import { StyleSheet, Text, View } from "react-native";
-import { cruxPay } from "@cruxpay/js-sdk";
-// import * as blockstack from "blockstack";
+import { StyleSheet, Text, View, Button} from "react-native";
+
+import { encryption } from "@cruxpay/js-sdk/dist/cruxpay-sdk"
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
+
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       data: 'default value'
-    };
-    styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center"
-      }
-    });
-  }
-
-  componentDidMount(){
-    this.testCrux().then(res => {
-      alert('hello')
-    }).catch(err => {
-      alert(err);
-    })
+    }
   }
 
   async testCrux() {
-    await localStorage.setItem("cruxPay", JSON.stringify({
-      hello: "world"
-    }));
-    let a = await localStorage.getItem("cruxPay");
-    this.setState({data: a});
-    return a;
+      console.log("uuuuuuuuuuuuuuu")
+      console.log(encryption)
+      let encryptJSON = encryption.Encryption.encryptJSON;
+      let decryptJSON = encryption.Encryption.decryptJSON;
+      encryptJSON({'test':'test1'}, 'foo')
+          .then((encryptedValue)=>{
+              console.log(encryptedValue);
+              decryptJSON(encryptedValue.encBuffer, encryptedValue.iv, 'foo').then((decryptedValue)=>{
+                  console.log(decryptedValue);
+              })
+          }).catch((err) => {
+          console.log(err.errorCode);
+          console.log(err.message);
+      })
   }
 
-  // async cruxInit() {
-  //   let cruxClientOptions = {
-  //     getEncryptionKey: getPassHashedEncryptionKey,     
-  //     walletClientName: 'cruxdev'
-  //   }
-  //   let cruxClient = new cruxPay.CruxClient(cruxClientOptions);
-  //   cruxClient.init()
-  //   .then(() => {
-  //     console.log('CruxClient initialized');
-  //   }).catch((err) => {
-  //     console.log('CruxClient error', err);
-  //   })
-  // }
+  componentDidMount() {
+    this.testCrux().then(res => {
+      console.log('hello');
+    }).catch(err => {
+      console.log(err);
+    })
+  }
 
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-        {/* <Text>{this.state.data}</Text> */}
+          <Text>
+              {this.state.data}
+          </Text>
+
       </View>
     );
   }
